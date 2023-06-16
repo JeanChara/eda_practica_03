831af7413dcad6ad624c722ad27f921c5900e291
JeanChara <jcharaco@unsa.edu.pe>
2023-06-16 11:42:13 -0500

    añadido metodo de eliminacion provicional

--- Ejercicio05/Heap.java
+++ Ejercicio05/Heap.java
@@ -1,5 +1,6 @@
 
 import java.util.*;
+// import exception
 
 public class Heap <T> {
     ArrayList<T> heapList;
@@ -8,6 +9,19 @@ public class Heap <T> {
     }
     public void insert(T item) {
         heap.add(item);
+        //
+    }
+    public T remove() {
+        if (isEmpty()) {
+            throw new ExceptionNoFound("No hay elementos en heap");
+        }
+
+        //
+
+        return null;//
+    }
+    public boolean isEmpty(){
+        return heapList.isEmpty();
     }
 
 }
