6613bb3603a2e222e32ce1b01b067e605e8fa410
Jean <jcharaco@unsa.edu.pe>
2023-06-16 23:18:37 -0500

    agregado y completado metodo HeapDown

--- Ejercicio05/Heap.java
+++ Ejercicio05/Heap.java
@@ -25,12 +25,31 @@ public class Heap <T extends Comparable <T>> {
         int indexPadre = (index-1)/2; // formula 
 
         // recorremos y comparamos los heap siempre y cuando halla mas de 1
-        while(index > 0 && heapList.get(index).compareTo(heapList.get(indexPadre)) > 0){ // heap maximo si es mayor, intercambiamos con el padre
+        while(index > 0 && heapList.get(index).compareTo(heapList.get(indexPadre)) > 0){ // heap maximo. si es mayor, intercambiamos con el padre
             intercambio(index,indexPadre);
             index = indexPadre; // cambiamos de nodo a comparar
             indexPadre = (index-1)/2; // re calculamos el nuevo index del padre
         }
 
+    }
+    private void headDown(int index){
+        int indexIzquierdo = 2 * index + 1;
+        int indexDerecho = 2 * index + 2;
+        int mayorIndex = index;
+
+        // TAD Heap maximo
+        if (indexIzquierdo < heapList.size() && heapList.get(indexIzquierdo).compareTo(heapList.get(mayorIndex)) > 0) {// vemos si nuestro nodo izquierdo es mayor a nuestro mayor nodo encontrado
+            mayorIndex = indexIzquierdo;// si es asi, reemplazamos el index
+        }
+
+        if (rightChildIndex < heapList.size() && heapList.get(indexDerecho).compareTo(heapList.get(mayorIndex)) > 0) {
+            mayorIndex = indexDerecho;
+        }
+
+        if (mayorIndex != index) {
+            swap(index, mayorIndex); // intercambiamos lugares 
+            heapifyDown(mayorIndex); // hasta que ninguno sea mayor que el elemento o que sea una hoja
+        }
     }
     private void intercambio(int i, int j){
         T aux = heapList.get(i);
