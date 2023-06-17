6feb10142895acc5c4e669a1485160cc74e4bc12
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-17 11:41:14 -0500

    PriorityQueue terminada

--- PriorityQueueHeap.java
+++ PriorityQueueHeap.java
@@ -8,12 +8,27 @@ public class PriorityQueueHeap<T extends Comparable<T>> {
 public void enqueue(T item,int prioridad) {
     heap.insert(item);
     //llama al metodo de la clase heap
+    
     }
     public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("La cola de prioridad está vacía.");
+        }
+
+        return heap.remove();
     }
     public T front() {
+        if (isEmpty()) {
+            throw new IllegalStateException("La cola de prioridad está vacía.");
+        }
+        return heap.getMax();
     }
     public T back() {
+        if (isEmpty()) {
+            throw new IllegalStateException("La cola de prioridad está vacía.");
+        }
+
+        return heap.getMin();
     }
     public boolean isEmpty() {
 
